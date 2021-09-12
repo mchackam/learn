f3d943753ddb889af56273838f3fed7bd7c326ce
mithun <mithuncmenon@gmail.com>
2021-09-12 18:02:21 +0530

    python requests

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello")
+#! /bin/python3
+import requests
+import json
+
+res = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+print(res.status_code)
+print(res.text)
+jsonObj = json.loads(res.text)
+print(jsonObj["title"])
