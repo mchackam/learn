65097e9ffb32cc8a78e357afee8c3f48356be7fc
mithun <mithuncmenon@gmail.com>
2021-09-12 18:11:18 +0530

    simple webserver

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+const http = require('http');
+
+const app = http.createServer((req,res) => {
+    res.writeHead(200,{"Content-Type":"application/json"});
+    const obj = {
+        name:"Mithun",
+        age:38
+    };
+    res.write(JSON.stringify(obj));
+    res.end();
+});
+
+app.listen(8080);
